f64e414c2c27aaf0d38bcea355aaf023ad24c534
Arslan Ali <ali.arslan319@gmail.com>
2019-04-14 01:30:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,3 +11,31 @@ def dictionary
   :at => "@",
   :and => "&"}
 end
+
+def word_substituter(tweet)
+  array = tweet.split(/ /)
+  dictionary.each do |key, value|
+    array.map { |string| string.downcase == key.to_s ? string.replace(value) : string }
+  end
+  array.join(" ") 
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    selective_tweet_shortener(tweet) 
+    tweet[0..136] << "..." 
+  else
+    tweet
+  end
+  
+end
